bc66c71fb85473e8a7e7a5372d2e2712a2870720
luzatti-alt <lucasguzatti4@gmail.com>
2025-10-01 21:13:23 -0300

    os includes de cpp e um codigo python

--- python/numpy/numpy_array.py
+++ python/numpy/numpy_array.py
@@ -1,1 +1,7 @@
 import numpy as np
+#numpy gera velocidade ao python
+#algumas funções de numpy são mais eficientes que a do python
+#um exemplo é o array
+np_array = np.array([1,2,3,4,5,6,7,8,9,10])#array mais efetivo em velocidade
+print(np_array,"\n")
+print(type(np_array),"isso é um array de numpy")
